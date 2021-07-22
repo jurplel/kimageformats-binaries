3250ee087d3dad76450e3bd0d60bb13d1c3ef8ce
Benjamin O <jeep70cp@gmail.com>
2021-07-22 14:51:53 -0400

    get vcvars suffix from OS arch

--- pwsh/vcvars.ps1
+++ pwsh/vcvars.ps1
@@ -1,6 +1,6 @@
 # Script assumes $env:arch will start with win64 or win32
 # This should probably be an arg
-$arch = $env:arch.substring(3, 2)
+$arch = if ([Environment]::Is64BitOperatingSystem) { "64" } else { "32" }
 $path = Resolve-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\*\*\VC\Auxiliary\Build" | select -ExpandProperty Path
 
 cmd.exe /c "call `"$path\vcvars$arch.bat`" && set > %temp%\vcvars.txt"
