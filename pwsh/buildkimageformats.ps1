e8baddde3ace19e1990909fa9b365b5b93ba0746
Benjamin O <jeep70cp@gmail.com>
2022-01-09 10:45:57 -0500

    Revert "try quite badly to link statically"
    This reverts commit 28f8899716aca3411af996a4568e428c740240f1.

--- pwsh/buildkimageformats.ps1
+++ pwsh/buildkimageformats.ps1
@@ -1,15 +1,12 @@
 #! /usr/bin/pwsh
 
-# remember to update modified cmakelists each version change
-$kde_vers = 'v5.90.0'
+$kde_vers = 'v5.89.0'
 
 # Clone
 git clone https://invent.kde.org/frameworks/kimageformats.git
-
-cp -f ./CMakeLists.txt kimageformats/src/imageformats/CMakeLists.txt
-
 cd kimageformats
 git checkout $kde_vers
+
 
 # Get dependencies
 if ($IsWindows) {
@@ -19,15 +16,15 @@ if ($IsWindows) {
     
     & "$env:GITHUB_WORKSPACE/pwsh/buildecm.ps1" $kde_vers
     & "$env:GITHUB_WORKSPACE/pwsh/buildkarchive.ps1"
+    & "$env:GITHUB_WORKSPACE/pwsh/buildopenexr.ps1"
     & "$env:VCPKG_ROOT/vcpkg.exe" install libheif libavif libjxl openexr
 } else {
     brew update
-    brew install nasm libheif openexr jpeg-xl libavif karchive
+    brew install nasm libheif openexr jpeg-xl libavif karchive extra-cmake-modules karchive
 
-    & "$env:GITHUB_WORKSPACE/pwsh/buildecm.ps1" $kde_vers
-
+    # extra-cmake-modules isn't on linuxbrew and I can't remember why ninja is done throught apt
     if ($IsMacOS) {
-        brew install ninja 
+        brew install ninja extra-cmake-modules 
     } else {
         $env:PKG_CONFIG_PATH += ":/home/linuxbrew/.linuxbrew/lib/pkgconfig"
         sudo apt-get install ninja-build
