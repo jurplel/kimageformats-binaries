53c6310d2ca6e8deed9d0498742c5e4af5349af8
Benjamin O <jeep70cp@gmail.com>
2022-01-08 21:06:24 -0500

    Try to switch to all brew

--- pwsh/buildkimageformats.ps1
+++ pwsh/buildkimageformats.ps1
@@ -20,24 +20,15 @@ if ($IsWindows) {
     & "$env:VCPKG_ROOT/vcpkg.exe" install libheif libavif libjxl openexr
 } else {
     brew update
-    brew install nasm libheif openexr jpeg-xl
+    brew install nasm libheif openexr jpeg-xl libavif karchive extra-cmake-modules karchive
 
     # extra-cmake-modules isn't on linuxbrew and I can't remember why ninja is done throught apt
     if ($IsMacOS) {
-        brew install ninja extra-cmake-modules karchive
+        brew install ninja extra-cmake-modules 
     } else {
         $env:PKG_CONFIG_PATH += ":/home/linuxbrew/.linuxbrew/lib/pkgconfig"
-        brew install openexr
         sudo apt-get install ninja-build
-        & "$env:GITHUB_WORKSPACE/pwsh/buildecm.ps1"
-        & "$env:GITHUB_WORKSPACE/pwsh/buildkarchive.ps1"
     }
-
-    # Build libavif dependency
-
-    & "$env:GITHUB_WORKSPACE/pwsh/buildlibavif.ps1"
-
-    $env:libavif_DIR = "libavif/build/installed/usr/local/lib/cmake/libavif/"
 }
 
 
@@ -56,12 +47,12 @@ ninja
 
 # Move some dependencies around
 if ($IsMacOS) {
-    mv libavif/build/installed/usr/local/lib/libavif.dylib ./bin/
+    mv libavif/build/installed/usr/local/lib/libavif.dylib kimageformats/bin/
 
-    mv karchive/bin/*.dylib ./bin/
+    mv karchive/bin/*.dylib kimageformats/bin/
 } elseif ($IsLinux) {
-    mv libavif/build/installed/libavif.so ./bin/
-    mv karchive/bin/*.so ./bin/
+    mv libavif/build/installed/libavif.so kimageformats/bin/
+    mv karchive/bin/*.so kimageformats/bin/
 } elseif ($IsWindows) {
     mv kimageformats/bin/imageformats/aom.dll kimageformats/bin/
     mv kimageformats/bin/imageformats/avif.dll kimageformats/bin/
