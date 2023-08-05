75e78daef3fc40de4cc55f72986ae0d1e744d475
Benjamin Owad <benjamin@bodo.ai>
2023-08-04 23:24:50 -0400

    let's try again

--- pwsh/get-vcpkg-deps.ps1
+++ pwsh/get-vcpkg-deps.ps1
@@ -57,7 +57,8 @@ if ($IsWindows) {
 
 # Build arm64-osx dependencies separately--we'll have to combine stuff later.
 if ($env:universalBinary) {
-    brew uninstall --ignore-dependencies libheif # Avoid linking to /usr/local/libsharpyuv
+    brew uninstall --ignore-dependencies libwebp # Avoid linking to /usr/local/libsharpyuv
+    ls -h /usr/local/lib/
     sudo rm -f /usr/local/lib/libsharpyuv.dylib
     & "$env:VCPKG_ROOT/$vcpkgexec" install --keep-going libjxl:arm64-osx libavif:arm64-osx openexr:arm64-osx zlib:arm64-osx
 }
