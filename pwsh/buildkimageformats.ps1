61dffb2722cc3cb2ec924a5040206dd7bd88546f
Benjamin O <jeep70cp@gmail.com>
2023-08-09 22:18:03 -0400

    Copy the rest of the dlls on windows

--- pwsh/buildkimageformats.ps1
+++ pwsh/buildkimageformats.ps1
@@ -104,6 +104,7 @@ if ($env:universalBinary) {
     # Copy karchive stuff to output as well
     if ($IsWindows) {
         cp karchive/bin/*.dll $prefix_out
+        cp "$env:VCPKG_ROOT/installed/$env:VCPKG_DEFAULT_TRIPLET/bin/*.dll" $prefix_out
     } elseif ($IsMacOS) {
         cp karchive/bin/*.dylib $prefix_out
     } else {
