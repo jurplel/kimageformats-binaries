f8a488400805b0d480b9cef194cd2e62ec110436
J.D. Purcell <jdpurcell@gmail.com>
2024-10-14 00:21:41 -0400

    macOS/Qt5: Fix HEIF

--- pwsh/get-vcpkg-deps.ps1
+++ pwsh/get-vcpkg-deps.ps1
@@ -21,6 +21,9 @@ if ($IsWindows) {
     choco install nasm
 } elseif ($IsMacOS) {
     brew install nasm
+    # Uninstall this because it can result in a non-working heif plugin,
+    # but it might not be present, so silence stderr
+    brew uninstall --ignore-dependencies webp 2>$null
 } else {
     # (and bonus dependencies)
     sudo apt-get install nasm libxi-dev libgl1-mesa-dev libglu1-mesa-dev mesa-common-dev libxrandr-dev libxxf86vm-dev
